42c33c3e6dfce63ad7c0206d4446075247429db2
aalesv <aalesv70@hotmail.com>
2024-06-01 13:40:47 +0300

    Cleanup

--- LibScoobyRom/RomChecksumming.cs
+++ LibScoobyRom/RomChecksumming.cs
@@ -30,7 +30,6 @@ namespace Subaru
 	public class RomChecksumming
 	{
 		public const int SizeOfNativeStruct = 3 * sizeof(UInt32);
-		//public const int ChecksumTableRecordCount = 17;
 		public const int ChecksumConstant = 0x5AA5A55A;
 
 		int[] tablePos;
@@ -101,29 +100,21 @@ namespace Subaru
 
 		public IList<RomChecksumRecord> ReadTableRecords ()
 		{
-			//return ReadTableRecords (this.tablePos, ChecksumTableRecordCount);
 			List<RomChecksumRecord> records = new List<RomChecksumRecord> ();
 			for (int i = 0; i < this.tablePos.Length; i++) {
 				IList<RomChecksumRecord> r = ReadTableRecords(this.tablePos[i],
 													this.ChecksumTableRecordCount()[i]
 												);
-				//records = records.Concat(r);
 				records.AddRange(r);
 			}
 			return records;
 		}
-
-		/*public IList<RomChecksumRecord> ReadTableRecords (int count)
-		{
-			return ReadTableRecords (this.tablePos, count);
-		}*/
 
 		public IList<RomChecksumRecord> ReadTableRecords (int pos, int count)
 		{
 			List<RomChecksumRecord> records = new List<RomChecksumRecord> (count);
 
 			int offset = Rom.RomLoadAddress(this.romType);
-			//stream.Seek (tablePos, System.IO.SeekOrigin.Begin);
 			stream.Seek (pos, System.IO.SeekOrigin.Begin);
 			for (int i = 0; i < count; i++) {
 				int _start 	  = stream.ReadInt32BigEndian (),
